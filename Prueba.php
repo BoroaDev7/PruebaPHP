16baf31ad0c582bc43c193dd13a6518f198f692e
BoroaDev7 <jmborjas@unitec.edu>
2023-04-28 08:53:58 -0600

    Primer

---
+++ Prueba.php
@@ -0,0 +1,14 @@
+  <?php
+    
+  $argConsola=count($argv);
+    function dificil(){
+      echo "Ni modo pa la vida es dificil\n";
+      echo "Nose que hacer para subir a GitHub\n";
+    }
+    $nombre =$argv[1];
+    $edad=$argv[2];
+    
+    dificil();
+    
+    echo"Hola $nombre tienes $edad años";
+    ?>
